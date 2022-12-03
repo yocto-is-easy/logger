fb300b43990cdf1a941df81d220adf2bbcce0788
Mykhailo Didur <mikhaildidur2003@gmail.com>
2022-12-03 16:14:42 +0200

    Separate logs to /var/mocto/log file
    Also add timestamps with acquracy up to nanoseconds.
    
    Signed-off-by: Mykhailo Didur <mikhaildidur2003@gmail.com>

--- src/impl/file_logger.hpp
+++ src/impl/file_logger.hpp
@@ -1,15 +1,39 @@
 #pragma once
 
+#include <ctime>
 #include <string>
+#include <chrono>
 #include <fstream>
+#include <sstream>
 #include <filesystem>
 
 namespace fs = std::filesystem;
+namespace chrono = std::chrono;
 
 class file_logger
 {
 private:
     std::string m_logFilePath;
+
+    std::string get_timestamp() {
+        auto now = chrono::system_clock::now();
+
+        auto ms = chrono::duration_cast<chrono::milliseconds>(now.time_since_epoch()) % 1000;
+        auto mcs = chrono::duration_cast<chrono::microseconds>(now.time_since_epoch()) % 1000;
+        auto ns = chrono::duration_cast<chrono::nanoseconds>(now.time_since_epoch()) % 1000;
+
+        std::time_t t = std::time(nullptr);
+        std::tm tm = *std::gmtime(&t);
+
+        std::stringstream ss;
+        ss << std::put_time(&tm, "%D %T");
+        ss << "." << std::setfill('0') << std::setw(3) << ms.count();
+        ss << "." << std::setfill('0') << std::setw(3) << mcs.count();
+        ss << "." << std::setfill('0') << std::setw(3) << ns.count();
+
+        return ss.str();
+    }
+
 public:
     file_logger(std::string logFilePath = "/dev/kmsg")
         : m_logFilePath(logFilePath) {
@@ -22,9 +46,10 @@ public:
     void log(std::string msg) {
         std::ofstream logFile;
 
-        logFile.open(m_logFilePath);
+        logFile.open(m_logFilePath, std::fstream::app);
         if(logFile.is_open()) {
-            logFile << (msg + "\n");
+            std::string data = "[" + get_timestamp() + "] " + msg + "\n";
+            logFile << data;
             logFile.close();
             return;
         }
--- src/impl/main.cpp
+++ src/impl/main.cpp
@@ -2,11 +2,13 @@
 
 #include "file_logger.hpp"
 
+const std::string DEFAULT_LOG_FILEPATH = "/var/mocto/log";
+
 INIT_MIDF_SERVER(logger)
 
 MIDF_IMPL_FUNC(bool, logger, log, std::string/*who*/, std::string/*message*/)
     (std::string who, std::string msg) {
-    static file_logger fl;
+    static file_logger fl(DEFAULT_LOG_FILEPATH);
     fl.log(who + ": " + msg);
 
     return true;
